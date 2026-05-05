2ef853c8e47160825e68ff29b5b0aba0d441fbca
Karoline Boddum <eaa24kalb@students.eaaa.dk>
2026-05-05 11:51:09 +0200

    .2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styles from "./Header.module.css";
 import NavOverlay from "./NavOverlay";
 import logoWhite from "../assets/images/logo-white.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
@@ -9,9 +10,9 @@ export default function Header() {
   return (
     <>
       <header className={styles.header}>
-        <a href="/" className={styles.logoLink}>
-          <img src={logoWhite} alt="Naturhistorisk Museum Aarhus" />
-        </a>
+        <Link to="/" className={styles.logoLink}>
+  <img src={logoWhite} alt="Naturhistorisk Museum Aarhus" />
+</Link>
 
         <button
           className={styles.menuBtn}
